d7ff445dbc104cd9c5a29746f987dc9aedbfab50
Jamie Davidson <JamieDavidson@users.noreply.github.com>
2024-05-02 12:43:16 +0100

    Undo path changes in dailyscape.js

--- dailyscape.js
+++ dailyscape.js
@@ -438,7 +438,7 @@ const populateTable = function(timeFrame) {
                     let itemInputData = !!item.inputs ? ' data-inputs="' + encodeURIComponent(JSON.stringify(item.inputs)) + '"' : '';
 
                     newRowColor.innerHTML += '<div class="item_output" data-item_id="' + item.id + '" data-shop_price="' + item.shop_price + '"' + itemInputData + '>'
-                                            + '<img class="item_icon" src="https://dailyscape.github.io/rsdata/images/' + item.id + '.gif">'
+                                            + '<img class="item_icon" src="/rsdata/images/' + item.id + '.gif">'
                                             + (!!item.label_override ? item.label_override : itemApiData.name) + ' x' + item.quantity.toLocaleString() + ' (' + item.profit.toLocaleString() + ')'
                                             + '</div>';
                 }
@@ -448,7 +448,7 @@ const populateTable = function(timeFrame) {
                     for (let item of skipItems) {
                         let itemApiData = rsapidata[item.id];
                         newRowColor.innerHTML += '<div class="item_output" data-item_id="' + item.id + '" data-shop_price="' + item.shop_price + '">'
-                                                + '<img class="item_icon" src="https://dailyscape.github.io/rsdata/images/' + item.id + '.gif">'
+                                                + '<img class="item_icon" src="/rsdata/images/' + item.id + '.gif">'
                                                 + (!!item.label_override ? item.label_override : itemApiData.name) + ' x' + item.quantity.toLocaleString() + ' (' + item.profit.toLocaleString() + ')'
                                                 + '</div>';
                     }
@@ -461,7 +461,7 @@ const populateTable = function(timeFrame) {
                     let itemInputData = !!item.inputs ? ' data-inputs="' + encodeURIComponent(JSON.stringify(item.inputs)) + '"' : '';
 
                     newRowColor.innerHTML += '<div class="item_output" data-item_id="' + item.id + '" data-shop_price="' + item.shop_price + '"' + itemInputData + '>'
-                                            + '<img class="item_icon" src="https://dailyscape.github.io/rsdata/images/' + item.id + '.gif">'
+                                            + '<img class="item_icon" src="/rsdata/images/' + item.id + '.gif">'
                                             + (!!item.label_override ? item.label_override : itemApiData.name) + ' x' + item.quantity.toLocaleString()
                                             + '</div>';
                 }
@@ -992,9 +992,9 @@ const warbandsCounter = function() {
 
     const outputElement = document.getElementById('traveling-merchant-stock');
     outputElement.innerHTML = '<br><strong>Current stock:</strong><br>';
-    outputElement.innerHTML += '<img class="item_icon" src="https://dailyscape.github.io/rsdata/images/' + slotA + '.gif"> ' + merchantitems[slotA].name + '<br>';
-    outputElement.innerHTML += '<img class="item_icon" src="https://dailyscape.github.io/rsdata/images/' + slotB + '.gif"> ' + merchantitems[slotB].name + '<br>';
-    outputElement.innerHTML += '<img class="item_icon" src="https://dailyscape.github.io/rsdata/images/' + slotC + '.gif"> ' + merchantitems[slotC].name;
+    outputElement.innerHTML += '<img class="item_icon" src="/rsdata/images/' + slotA + '.gif"> ' + merchantitems[slotA].name + '<br>';
+    outputElement.innerHTML += '<img class="item_icon" src="/rsdata/images/' + slotB + '.gif"> ' + merchantitems[slotB].name + '<br>';
+    outputElement.innerHTML += '<img class="item_icon" src="/rsdata/images/' + slotC + '.gif"> ' + merchantitems[slotC].name;
 };
 
 /**
@@ -1189,7 +1189,7 @@ const itemStatsTooltip = function() {
 
             item.after(tooltip);
 
-            tooltip.innerHTML = '<img src="https://dailyscape.github.io/rsdata/images/' + this.dataset.item_id + '.gif" class="item_icon"> ' + itemdata.name + '<br>'
+            tooltip.innerHTML = '<img src="/rsdata/images/' + this.dataset.item_id + '.gif" class="item_icon"> ' + itemdata.name + '<br>'
                                 + 'GE: ' + itemdata.price.toLocaleString() + '<span class="coin">●</span>' + (parseInt(this.dataset.shop_price) > 0 ? ' Shop: ' + parseInt(this.dataset.shop_price).toLocaleString() + '<span class="coin">●</span>' : '');
             tooltip.innerHTML += '<br>Change: ' + (itemdata.price > itemdata.last ? '+' : '') + (itemdata.last != itemdata.price ? (itemdata.price - itemdata.last).toLocaleString() : '-') + (itemdata.price > itemdata.last ? '<span class="trend_positive">▲</span>' : itemdata.price < itemdata.last ? '<span class="trend_negative">▼</span>' : '<span class="trend_neutral">-</span>');
 
@@ -1200,7 +1200,7 @@ const itemStatsTooltip = function() {
 
                 for (let itemkey in inputItems) {
                     let inputItemData = rsapidata[itemkey];
-                    tooltip.innerHTML += ' <img src="https://dailyscape.github.io/rsdata/images/' + itemkey + '.gif" class="item_icon"> ' + inputItemData.name + ' x' + inputItems[itemkey] + ' (-' + parseInt(inputItemData.price * inputItems[itemkey]).toLocaleString() + ')<br>';
+                    tooltip.innerHTML += ' <img src="/rsdata/images/' + itemkey + '.gif" class="item_icon"> ' + inputItemData.name + ' x' + inputItems[itemkey] + ' (-' + parseInt(inputItemData.price * inputItems[itemkey]).toLocaleString() + ')<br>';
                 }
 
             }
@@ -1251,7 +1251,7 @@ const dataUpdatedCheck = function() {
         }
     };
 
-    xmlhttp.open("GET", "https://dailyscape.github.io/rsdata/rsapiupdated.json", true);
+    xmlhttp.open("GET", "/rsdata/rsapiupdated.json", true);
     xmlhttp.send();
 }
 
